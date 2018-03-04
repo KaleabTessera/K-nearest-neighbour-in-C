54dbf2a3524cbe006771a204945737cc07519e09
Unknown <kaleabtessera@gmail.com>
2018-03-04 21:41:20 +0200

    Worked on serial version of calculating distance for knn.

--- main.c
+++ main.c
@@ -1,38 +1,84 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+int dimension = 0;
+int numElementsQueryArray = 0;
+int numElementsReferenceArray = 0;
 
-double * getDistances(int qi, double * pj){
-    return pj;
-}
+void printArray( double * pj[], int numElements,int dimension);
+double** getDistances(double* qiArray[], double * pjArray[]);
+double* getDistancePerQueryPoint(double * qi, double * pjArray[], int pjArrayLength);
+double euclideanDistance(double * qi, double * pj);
+int main() {
     
-void printArray( double * pj, int numElements){
-    for(int index = 0; index < numElements; index++){
-        printf( "%lf", pj[index] );
-        printf( "\n" );
-    }
+    numElementsQueryArray = 2;
+    numElementsReferenceArray = 2;
+    dimension = 2; 
+
+    double * qiArray[numElementsQueryArray];
+    double * pjArray[numElementsReferenceArray];
+
+    double qi[2] = {5,6};
+    double qi2[2] = {7,8};
+
+    double pj[2] = {10,1.5};
+    double pj2[2] = {11,2.5};
+
+    qiArray[0] = qi;
+    qiArray[1] = qi2;
+    pjArray[0] = pj;
+    pjArray[1] = pj2;
+
+    double** array = getDistances(qiArray,pjArray);
+     printf( "%lf", array[0][0] );
+    
+    return 0;
 }
 
-double euclideanDistance(int dimention, double * qi, double * pj){
+void printArray( double * array[], int numElements,int dimension){
+    printf( "[");
+    for(int x = 0; x < numElements; x++){
+        printf( "[" );
+        for(int y = 0; y < dimension; y++){
+            printf( "%lf", array[x][y] );
+            if(x != numElements -1){
+                printf(",");
+            }
+        }
+        printf( "]" );
+        if(x != numElements -1){
+            printf(",");
+        }
+    }
+    printf("] \n");
+}
+
+double** getDistances(double* qiArray[], double * pjArray[]){
+    double** euclideanDistanceArray= (double**)malloc(sizeof(double*)*numElementsReferenceArray);
+    for(int index = 0 ; index < numElementsQueryArray; index++){
+        euclideanDistanceArray[index] = getDistancePerQueryPoint(qiArray[index],pjArray,numElementsReferenceArray);
+    }
+    return euclideanDistanceArray;
+}
+
+double* getDistancePerQueryPoint(double * qi, double * pjArray[], int pjArrayLength){
+
+    double *distanceArray = (double*)malloc(sizeof(double)*pjArrayLength);
+    for(int index = 0 ; index < pjArrayLength; index++){
+        distanceArray[index] = euclideanDistance(qi,pjArray[index]);
+    }
+    return distanceArray;
+}
+
+
+double euclideanDistance(double * qi, double * pj){
     int distance = 0;
-    for(int index = 0; index < dimention; index++){
+    for(int index = 0; index < dimension; index++){
             distance += pow((qi[index] - pj[index]),2);
     }
     return sqrt(distance);
 }
 
-int main() {
-    int numElements;
-    numElements = 100;
 
-    double pj[numElements];
-    double qi2[4] = {5,7,8,9};
-    double pj2[4] = {10,1.5,3.8,9.7};
-    for(int i = 0; i < numElements; i++){
-        pj[i] = 2;
-    }
-    double distance = euclideanDistance(4,qi2,pj2);
-    printf("%lf", distance );  
-    //printArray(pj,numElements) ;
-    return 0;
-}
+
 
