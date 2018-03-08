8726a8f80b55b8dd403c2b86857daddafa065b8b
Unknown <kaleabtessera@gmail.com>
2018-03-08 14:54:50 +0200

    Complemented serial implementation and pointed to mnist dataset.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int dimension = 0;
 int numColumns = 0;
 int numberOfElementsInQuerySet = 0;
 int numberOfElementsInReferenceSet = 0;
+int k;
 
 enum ElementType { et_str, et_int, et_dbl };
 typedef struct Element {
@@ -13,47 +14,44 @@ typedef struct Element {
     enum ElementType type;
     double inputs[];
 } record_t;
+
+typedef struct ArrayElement{
+    double output;
+    enum ElementType type;
+    double inputs[];
+} array_element ;
     
 
-void printArray( double * pj[], int numElements,int dimension);
-double** getDistances(record_t** qiArray, record_t ** pjArray);
-double* getDistancePerQueryPoint(record_t * qi, record_t ** pjArray, int pjArrayLength);
+//void printArray( double ** pj, int numElements,int dimension);
+void printArray( double ** array,int dimension);
+void getDistances(record_t** qiArray, record_t ** pjArray);
+double** getDistancePerQueryPoint(record_t * qi, record_t ** pjArray, int pjArrayLength);
 double euclideanDistance(double * qi, double * pj);
 void quickSort(double ** array, int arraySize);
+//int indexOfElement(int val, int *arr, int size);
 
-
-const char* getfield(char* line, int num)
-{
-    const char* tok;
-    for (tok = strtok(line, ";");
-            tok && *tok;
-            tok = strtok(NULL, ";\n"))
-    {
-        if (!--num)
-            return tok;
-    }
-    return NULL;
-}
 
 //https://www.tutorialspoint.com/c_standard_library/c_function_qsort.htm
-int cmpfunc (const void * a, const void * b) {
-   return ( *(double*)a - *(double*)b );
+int cmpfunc (const void ** a, const void ** b) {
+   return ( *(double*)a[0] - *(double*)b[0] );
 }
 
 int main() {
 
-    FILE* stream = fopen("winequality-red-large.csv", "r");
-    char line[1024];
+    FILE* stream = fopen("mnist_train.csv", "r");
+    char line[2048];
     
     int lineNumber = 0;
-    int maxNumberOfElements = 10;
+    int maxNumberOfElements = 1000;
+    int indexOfOutput = 0;
+    k = 5;
 
     //+1 to cater for first line with feature headings
     int maxNumLinesRead = maxNumberOfElements + 1;
     //Percentage of data placed in reference set
     double percentageReferenceSet = 0.8;
 
-    numColumns = 12;
+    numColumns = 10000;
     //dimension excludes output column
     dimension = numColumns-1;
     numberOfElementsInReferenceSet = (int)(percentageReferenceSet * maxNumberOfElements);
@@ -64,14 +62,14 @@ int main() {
     
     size_t count = 0;
 
-    const char delimeter[2] = ";";
+    const char delimeter[2] = ",";
     
    
     int indexReferenceArray = 0;
     int indexQueryArray = 0;
 
     //https://stackoverflow.com/questions/12911299/read-csv-file-in-c
-    while (fgets(line, 1024, stream) && (lineNumber < maxNumLinesRead))
+    while (fgets(line, 2048, stream) && (lineNumber < maxNumLinesRead))
     {
         if(lineNumber > 0){
             char* tmp = strdup(line);
@@ -81,9 +79,9 @@ int main() {
             char* token;
             token = strtok(tmp, delimeter);
             // https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
-            while( token != NULL ) {
+            while( token != NULL && column < numColumns) {
                 if(lineNumber > 0){
-                    if(column == numColumns-1){
+                    if(column == indexOfOutput){
                         record->output = strtod (token, NULL);    
                     }
                     else record->inputs[column] = strtod (token, NULL);
@@ -102,43 +100,85 @@ int main() {
                 indexQueryArray = indexQueryArray+1;
             } 
             free(tmp);
-            free(token);
+            //free(token);
            // free(record);
         }
         lineNumber = lineNumber+1;
     }
     printf("%s %d \n","Reference Set Size:",numberOfElementsInReferenceSet );
-    printStruct(pjArray,numberOfElementsInReferenceSet,dimension);
+    //printStruct(pjArray,numberOfElementsInReferenceSet,dimension);
     printf("%s %d \n","Query Set Size:",numberOfElementsInQuerySet);
-    printStruct(qiArray,numberOfElementsInQuerySet,dimension);
+    //printStruct(qiArray,numberOfElementsInQuerySet,dimension);
 
-     double** array = getDistances(qiArray,pjArray);
-     printf("%s","Distance array: ");
-     printArray(array,numberOfElementsInQuerySet,numberOfElementsInReferenceSet);
-     quickSort(array,numberOfElementsInQuerySet);
-     printf("%s","Sorted distance array: ");
-     printArray(array,numberOfElementsInQuerySet,numberOfElementsInReferenceSet);
-    
-     //findNeighbours(array,3);
-     return 0;
+    getDistances(qiArray,pjArray);
+
+    return 0;
 }
 
-void printArray( double ** array, int numElements,int dimension){
-    printf( "[");
-    for(int x = 0; x < numElements; x++){
+
+//https://stackoverflow.com/questions/15094834/check-if-a-value-exist-in-a-array
+int indexOfElement(int val, double **arr, int size){
+    int i;
+    for (i=0; i < size; i++) {
+        if (arr[i][0] == val)
+            return i;
+    }
+    return -1;
+}
+
+int indexOfLargestElement( double ** arr, int size){
+    double largest = arr[0][1];
+    int index =0;
+    for (int i = 1; i < size; i++) 
+    {
+	    if (largest< arr[i][1]){
+	        largest = arr[i][1];
+            index = i;
+        }
+	}
+    return index;
+}
+
+void findNeighbours(double **array, int k, double actualOutput){
+    printf("%s %d \n","Finding neighbours with k: ",k);
+    double predictedOutputs[k];
+    //double** countOccurences= (double*)malloc(sizeof(double)*dimension);;
+
+    double **countOccurences = (double **)malloc(k * sizeof(double *));
+    for (int i=0; i<k; i++)
+         countOccurences[i] = (double *)malloc(2 * sizeof(int));
+
+    int indexInOccurenceArray;
+    for(int i=0; i< k;i++){
+        predictedOutputs[i] = array[i][1];
+        printf("%s %d %s %lf","Predicted output, neighbour ",i,":" ,predictedOutputs[i]);
+        printf(" \n");
+        
+        indexInOccurenceArray =  indexOfElement(predictedOutputs[i],countOccurences,k);
+        if(indexInOccurenceArray == -1){
+            countOccurences[i][0] = predictedOutputs[i];
+            countOccurences[i][1] = 1;
+        } else {
+            countOccurences[indexInOccurenceArray][0] = predictedOutputs[i];
+            countOccurences[indexInOccurenceArray][1] =  countOccurences[indexInOccurenceArray][1] +1;
+        }
+        
+    }
+    int indexLargest = indexOfLargestElement(countOccurences,k);
+    printf("\nAssigned output:  %lf", countOccurences[indexLargest][0]);
+    printf("\nActual output:  %lf", actualOutput);
+    printf("%s","\n");
+}
+
+void printArray( double ** array,int dimension){
         printf( "[" );
-        for(int y = 0; y < dimension; y++){
-            printf( "%lf", array[x][y] );
-            if(y != dimension -1){
+        for(int x = 0; x < dimension; x++){
+            printf( "%lf", array[x][0] );
+            if(x != dimension -1){
                 printf(",");
             }
         }
         printf( "] \n" );
-        if(x != numElements -1){
-            printf(",");
-        }
-    }
-    printf("] \n");
 }
 
 void printStruct( record_t ** array, int numElements,int dimension){
@@ -167,19 +207,40 @@ void quickSort(double ** array, int arraySize){
     }
 }
 
-double** getDistances(record_t** qiArray, record_t ** pjArray){
-    double** euclideanDistanceArray= (double**)malloc(sizeof(double*)*numberOfElementsInReferenceSet);
+
+void getDistances(record_t** qiArray, record_t ** pjArray){
+    double*** euclideanDistanceArray= (double***)malloc(sizeof(double**)*numberOfElementsInReferenceSet);
+    //Step 0 & 4. repeat steps 1 to 3 for q i+1 .
     for(int index = 0 ; index < numberOfElementsInQuerySet; index++){
+        printf("%s","\n");
+        //Step 1 compute all the distances between q i and p j , 0 ≤ j ≤ m;
+        //printf("%s \n","Step 1 compute all the distances between q i and p j , 0 ≤ j ≤ m;");
         euclideanDistanceArray[index] = getDistancePerQueryPoint(qiArray[index],pjArray,numberOfElementsInReferenceSet);
+        //printf("%s","Distance array: ");
+        //printArray(euclideanDistanceArray[index],numberOfElementsInReferenceSet);
+        
+        //Step 2 sort the computed distances;
+        //printf("\n%s","Step 2 sort the computed distances; \n");
+        qsort(euclideanDistanceArray[index], numberOfElementsInReferenceSet, sizeof(double*), cmpfunc);
+        //printf("%s","Sorted distance array: ");
+        //printArray(euclideanDistanceArray[index],numberOfElementsInReferenceSet);
+        
+        //printf("\n%s","Step 3 select the k reference points corresponding to the k smallest distances \n");
+        //Step 3 select the k reference points corresponding to the k smallest distances;
+        findNeighbours(euclideanDistanceArray[index],k,qiArray[index]->output);
     }
     return euclideanDistanceArray;
 }
 
-double* getDistancePerQueryPoint(record_t * qi, record_t ** pjArray, int pjArrayLength){
 
-    double *distanceArray = (double*)malloc(sizeof(double)*pjArrayLength);
+double** getDistancePerQueryPoint(record_t * qi, record_t ** pjArray, int pjArrayLength){
+    double **distanceArray = (double **)malloc(pjArrayLength * sizeof(double *));
+    for (int i=0; i<pjArrayLength; i++)
+         distanceArray[i] = (double *)malloc(2 * sizeof(double));
+
     for(int index = 0 ; index < pjArrayLength; index++){
-        distanceArray[index] = euclideanDistance(qi->inputs,pjArray[index]->inputs);
+        distanceArray[index][0] = euclideanDistance(qi->inputs,pjArray[index]->inputs);
+        distanceArray[index][1] = pjArray[index]->output; 
     }
     return distanceArray;
 }
